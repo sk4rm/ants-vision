5ae7deebad19aea9983f09221b3136c051311916
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-08-16 12:04:45 -0700

    mqtt: Implement basic client

--- ocr.py
+++ ocr.py
@@ -1,12 +1,33 @@
 import cv2
-from paddleocr import PaddleOCR
 import pyttsx3
+from paddleocr import PaddleOCR
+import paho.mqtt.client as mqtt
+
+MQTT_BROKER_HOST = "localhost"
+MQTT_VOICE_TOPIC = "services/voice"
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}.")
+    client.subscribe(MQTT_VOICE_TOPIC)
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
 
 ocr = PaddleOCR()
 
 cap = cv2.VideoCapture(0)  # 0 = default webcam
 print("Press SPACE to capture an image, ESC to quit.")
 engine = pyttsx3.init()
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.enable_logger()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect(MQTT_BROKER_HOST)
+mqttc.loop_start()
 
 while True:
     ret, frame = cap.read()
@@ -21,11 +42,11 @@ while True:
     # SPACE = capture image
     if key == 32:
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
+
         results = ocr.predict(rgb)
-        
+
         for res in results:
-            alltext = results[0]['rec_texts']
+            alltext = results[0]["rec_texts"]
             print(alltext)
             for text in alltext:
                 engine.say(text)
@@ -35,5 +56,7 @@ while True:
     elif key == 27:
         break
 
+mqttc.loop_stop()
+mqttc.disconnect()
 cap.release()
 cv2.destroyAllWindows()
