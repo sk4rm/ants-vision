976c2ac15c34101a6323d1f5db9b1563f7911a3a
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-08-16 21:12:02 -0700

    voice: Clean up STT output, implement basic MQTT client

--- voice.py
+++ voice.py
@@ -1,12 +1,14 @@
+import json
+import paho.mqtt.client as mqtt
 import queue
 import sounddevice as sd
 import sys
 
 from vosk import Model, KaldiRecognizer
 
+MQTT_BROKER_HOST = "localhost"
+MQTT_VOICE_TOPIC = "services/voice"
 q = queue.Queue()
-model = Model(lang="en-us")
-samplerate = sd.query_devices(kind="input")["default_samplerate"]
 
 
 def callback(indata, frames, time, status):
@@ -15,16 +17,42 @@ def callback(indata, frames, time, status):
     q.put(bytes(indata))
 
 
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code == mqtt.MQTT_ERR_SUCCESS:
+        print(f"Successfully connected to MQTT broker.")
+        client.subscribe(MQTT_VOICE_TOPIC)
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+model = Model(lang="en-us")
+samplerate = sd.query_devices(kind="input")["default_samplerate"]
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
 try:
+    mqttc.connect(MQTT_BROKER_HOST)
+    mqttc.loop_start()
+
     with sd.RawInputStream(callback=callback, dtype="int16", samplerate=samplerate):
         rec = KaldiRecognizer(model, samplerate)
-        print("ok")
+
+        print("Started recording microphone. Press Ctrl-C to stop.")
         while True:
             if rec.AcceptWaveform(q.get()):
-                print(rec.Result())
+                result = json.loads(rec.Result())
+                text = result["text"]
+                print(text if text != "" else "🤫")
 
 except KeyboardInterrupt:
     print("bye bye 👋")
 
 except Exception as e:
     print(f"uh oh 🙀 {e}")
+
+mqttc.loop_stop()
+mqttc.disconnect()
